5b43385fd6c0a80bba91c3012185938545f1845e
cheebou28 <cheebou28@gmail.com>
2020-03-26 13:11:22 +0900

    Signup and Login with firebase

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHightlight } from 'react-native';
+import firebase from 'firebase';
 
 class LoginScreen extends React.Component {
   state = {
@@ -8,8 +9,14 @@ class LoginScreen extends React.Component {
   }
 
   handleSubmit() {
-    console.log('submit');
-    // this.props.navigation.navigate('HOME')
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((user) => {
+        console.log('success!', user);
+        this.props.navigation.navigate('HOME')
+      })
+      .catch((error) => {
+        console.log('error!', error);
+      });
   }
 
   render() {
